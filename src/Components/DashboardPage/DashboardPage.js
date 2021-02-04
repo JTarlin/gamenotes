5b1f3d0906076edad52b2ef7dd997319147a05cd
Jackson Tarlin <jacksontarlin@gmail.com>
2021-02-04 09:44:44 -0800

    created basic header for dashboard

--- src/Components/DashboardPage/DashboardPage.js
+++ src/Components/DashboardPage/DashboardPage.js
@@ -1,8 +1,16 @@
+//component imports
+import Header from "../Header/Header";
 
 export default function DashboardPage() {
+
+    //make api call to backend for users, or to local storage for guest
+
     return (
-        <div>
-            
+        <div className="dashboardpage">
+            <Header />
+            {/* for once we have persistent data on the notes: */}
+            {/* <GameNotes /> */}
+
         </div>
     )
 }
