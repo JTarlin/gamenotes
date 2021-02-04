252a50be5df0136f08dbb679d68d45247b4ece52
Jackson Tarlin <jacksontarlin@gmail.com>
2021-02-03 17:39:23 -0800

    created sass partials to be used in the app

---
+++ src/Components/WelcomePage/WelcomePage.js
@@ -0,0 +1,15 @@
+//styling imports
+import "./WelcomePage.scss";
+
+export default function WelcomePage() {
+
+    return (
+        <div className="welcomepage">
+            <div className="welcomepage__bg"></div>
+            <div className="welcomepage__title">
+                GameNotes
+            </div>           
+        </div>
+        
+    )
+}
