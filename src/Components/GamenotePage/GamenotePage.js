5b06ec7491248e022a90f3ecb5421c50b48bf229
Jackson Tarlin <jacksontarlin@gmail.com>
2021-02-05 14:36:40 -0800

    canvas graph now populated from gamenotepages state

--- src/Components/GamenotePage/GamenotePage.js
+++ src/Components/GamenotePage/GamenotePage.js
@@ -16,11 +16,33 @@ export default function GamenotePage(props) {
     const history = useHistory();
     const [noteInfo, setNoteInfo] = useState(history.location.state);
 
+    //gameenotepage holds all the info for the network graph
+    const [graph, setGraph] = useState({nodes: [], edges: []})
+    if(!graph.nodes[0]){
+        console.log("empty");
+        setGraph({
+            nodes: [
+                { id: 1, label: 'Node 1' },
+                { id: 2, label: 'Node 2' },
+                { id: 3, label: 'Node 3' },
+                { id: 4, label: 'Node 4' },
+                { id: 5, label: 'Node 5' }
+            ],
+            edges: [
+                { from: 1, to: 2 },
+                { from: 1, to: 3 },
+                { from: 2, to: 4 },
+                { from: 2, to: 5 }
+            ]
+        })
+    }
+    
+
     return (
         <div className="gamenotepage">
             <Header />
             <section className="gamenote">
-                <NoteCanvas />
+                <NoteCanvas graph={graph} />
                 <InfoBlock noteInfo={noteInfo}/>
             </section>
         </div>
